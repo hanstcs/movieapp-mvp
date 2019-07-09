7c0e00cd4ec8ccfe51f6ef7f841c9561fce6c440
Hans Simorangkir <simorangkirhans@gmail.com>
2019-07-09 14:59:30 +0700

    Add fragment content to activity

--- app/src/main/java/com/hanstcs/movieapp/MoviesActivity.java
+++ app/src/main/java/com/hanstcs/movieapp/MoviesActivity.java
@@ -1,8 +1,14 @@
 package com.hanstcs.movieapp;
 
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+
+import static android.support.v4.util.Preconditions.checkNotNull;
 
 public class MoviesActivity extends AppCompatActivity {
 
@@ -12,6 +18,20 @@ public class MoviesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_movies);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
         setSupportActionBar(toolbar);
+
+        MoviesFragment moviesFragment =
+                (MoviesFragment) getSupportFragmentManager().findFragmentById(R.id.contentFrame);
+        if (moviesFragment == null) {
+            moviesFragment = MoviesFragment.newInstance();
+            addFragmentToActivity(moviesFragment);
+        }
+    }
+
+    private void addFragmentToActivity(MoviesFragment moviesFragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.add(R.id.contentFrame, moviesFragment);
+        transaction.commit();
     }
 }
---
+++ app/src/main/java/com/hanstcs/movieapp/MoviesFragment.java
@@ -0,0 +1,33 @@
+package com.hanstcs.movieapp;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MoviesFragment extends Fragment {
+
+    public MoviesFragment() {
+    }
+
+    public static MoviesFragment newInstance() {
+        return new MoviesFragment();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.movies_fragment, container, false);
+    }
+}
